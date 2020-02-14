24aa16e0a928c76adc2c5a53a577165a1130e138
Siqi-Xia <xsq1903@gmail.com>
2020-02-14 18:28:02 +1100

    models used
    Simple_model for detector

---
+++ models/simple_model.py
@@ -0,0 +1,37 @@
+"""
+A pure TensorFlow implementation of a convolutional neural network.
+"""
+# pylint: disable=missing-docstring
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import functools
+
+import tensorflow as tf
+
+from cleverhans import initializers
+from cleverhans.model import Model
+
+
+class SimpleLinear(Model):
+  def __init__(self, scope, nb_classes, nb_filters, **kwargs):
+    del kwargs
+    Model.__init__(self, scope, nb_classes, locals())
+    self.nb_filters = nb_filters
+
+    # Do a dummy run of fprop to make sure the variables are created from
+    # the start
+    self.fprop(tf.placeholder(tf.float32, [128, 28, 28, 1]))
+    # Put a reference to the params in self so that the params get pickled
+    self.params = self.get_params()
+
+  def fprop(self, x, **kwargs):
+    del kwargs
+    with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
+      logits = tf.layers.dense(
+          tf.layers.flatten(x), self.nb_classes,
+          kernel_initializer=initializers.HeReLuNormalInitializer)
+      return {self.O_LOGITS: logits,
+              self.O_PROBS: tf.nn.softmax(logits=logits)}
